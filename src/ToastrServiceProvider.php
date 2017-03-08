eb213836618bb1080982e6177aee488f6f49b06f
hacklee <517184100@qq.com>
2017-03-08 16:08:06 +0800

    fixed bug
    changed Toastr::class to 'toastr'

--- src/ToastrServiceProvider.php
+++ src/ToastrServiceProvider.php
@@ -28,7 +28,7 @@ class ToastrServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(Toastr::class,function ($app) {
+        $this->app->singleton('toastr',function ($app) {
             return new Toastr($app['session'], $app['config']);
         });
     }
